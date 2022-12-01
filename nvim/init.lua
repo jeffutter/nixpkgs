08e3e4ad7557fb00bac79156e346f159f3551c54
Jeffery Utter <jeff@jeffutter.com>
2022-12-01 09:24:30 -0600

    Adjust shade opacity in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -501,7 +501,7 @@ require("nvim-tree").setup()
 require("todo-comments").setup()
 
 require('shade').setup({
-  overlay_opacity = 50,
+  overlay_opacity = 75,
   opacity_step = 1,
   keys = {
     brightness_up   = '<C-Up>',
