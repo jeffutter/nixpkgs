c6d14d9bc7a09bc4430782fccf5121425a3db76d
Jeffery Utter <jeff@jeffutter.com>
2022-12-04 17:17:50 -0600

    Add trouble.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,6 +65,18 @@ require('packer').startup(function(use)
   }
 
   use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+  use {
     'vim-test/vim-test'
     -- config = function()
     --   vim.g['test#strategy'] = 'neovim'
