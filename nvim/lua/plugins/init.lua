15d72a7672543d016416018e6788167e9f263fcc
Jeffery Utter <jeff@jeffutter.com>
2023-01-26 07:21:43 -0600

    Update nvim indent blanklines

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -240,12 +240,17 @@ return {
     }
   },
 
+  -- indent guides for Neovim
   {
-    'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPost",
     opts = {
-      char = '┊',
+      -- char = "▏",
+      char = "│",
+      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
       show_trailing_blankline_indent = false,
-    }
+      show_current_context = false,
+    },
   },
 
   {
