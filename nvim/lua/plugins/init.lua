ccf98a8a65d3b91c0e5abbc23bec203f39149cd0
Jeffery Utter <jeff@jeffutter.com>
2023-10-11 22:55:34 -0500

    Update indent-blank-line

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -109,17 +109,7 @@ return {
 	},
 
 	-- indent guides for Neovim
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "BufReadPost",
-		opts = {
-			-- char = "▏",
-			char = "│",
-			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
-		},
-	},
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
 	{
 		"folke/flash.nvim",
