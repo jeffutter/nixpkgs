c48e695f644b3d946989f5fbe774c164cd2a39e8
Jeffery Utter <jeff@jeffutter.com>
2023-12-08 11:54:06 -0600

    Update tmux config

--- systems/common.nix
+++ systems/common.nix
@@ -536,16 +536,19 @@ in
   programs.tmux = {
     enable = true;
     escapeTime = 0;
+    historyLimit = 5000;
+    mouse = false;
     newSession = true;
     resizeAmount = 10;
+    shell = "${pkgs.fish}/bin/fish";
     shortcut = "a";
     terminal = "tmux-256color";
     extraConfig = ''
 set -ga terminal-overrides ",*256col*:Tc"
-set-option -g mouse off
-set-option -g default-command "fish"
 set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
 set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+set -g status-keys vi
+set -g mode-keys   vi
     '' + builtins.readFile (tokyonights + "/extras/tmux/tokyonight_moon.tmux");
     plugins = with pkgs.tmuxPlugins; [
       yank
