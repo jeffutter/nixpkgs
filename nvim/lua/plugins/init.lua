3f7336ff1a60d9f27c19cabdd6715687e67b46de
Jeffery Utter <jeff@jeffutter.com>
2023-12-27 20:10:26 -0600

    Add nvim action-preview

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -621,6 +621,29 @@ return {
 			"lewis6991/gitsigns.nvim",
 		},
 	},
+
+	{
+		"aznhe21/actions-preview.nvim",
+		config = function()
+			vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+
+			require("actions-preview").setup({
+				telescope = {
+					sorting_strategy = "ascending",
+					layout_strategy = "vertical",
+					layout_config = {
+						width = 0.8,
+						height = 0.9,
+						prompt_position = "top",
+						preview_cutoff = 20,
+						preview_height = function(_, _, max_lines)
+							return max_lines - 15
+						end,
+					},
+				},
+			})
+		end,
+	},
 }
 
 -- vim: ts=2 sts=2 sw=2 et
