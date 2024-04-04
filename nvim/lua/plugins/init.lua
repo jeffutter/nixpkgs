ac6edb42d014ac9059e4e63738a1edcea8a870da
Jeffery Utter <jeff@jeffutter.com>
2024-04-03 19:31:05 -0500

    Update nvim trouble to 3beta

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -401,15 +401,41 @@ return {
 
 	{
 		"folke/trouble.nvim",
+		branch = "dev", -- for v3
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
-			{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Trouble Toggle" },
-			{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Trouble Workspace" },
-			{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Trouble Document" },
-			{ "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Trouble LocList" },
-			{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Trouble Quickfix" },
-			{ "gR", "<cmd>TroubleToggle lsp_references<cr>", desc = "Trouble LSP References" },
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
 		},
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
 	},
 
 	{
