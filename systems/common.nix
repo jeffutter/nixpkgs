9d6bb354525a66e481208a9f987f6d00c3d0e70c
Jeffery Utter <jeff@jeffutter.com>
2024-05-20 15:56:11 -0500

    Add cachix

--- systems/common.nix
+++ systems/common.nix
@@ -95,6 +95,7 @@ in
       broot
       brotli
       bzip2
+      cachix
       comma
       curl
       difftastic
@@ -1037,6 +1038,8 @@ in
     package = pkgs.nix;
     extraOptions = ''
       experimental-features = nix-command flakes
+      substituters = https://cache.nixos.org https://jeffutter.cachix.org
+      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= jeffutter.cachix.org-1:ANzVqMBfIdjVJm1I7wAD/Dmr7hkqtsX6gWf+VXvC7Uw=
     '';
   };
 
