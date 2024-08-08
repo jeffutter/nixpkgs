2284aafa63e4b2f147619772cdd29795f00ce7ef
Jeffery Utter <jeff@jeffutter.com>
2024-08-08 15:30:00 -0500

    Add hyprlock config

--- systems/zenbook/home.nix
+++ systems/zenbook/home.nix
@@ -214,8 +214,130 @@ in
     };
   };
 
+  programs.hyprlock = {
+    enable = true;
+    # package = (nixGL pkgs.hyprlock);
+    # nix hyprlock doesn't play well with ubuntu pam
+    package = pkgs.runCommandLocal "empty" { } "mkdir $out";
+    settings = {
+
+      background = {
+        monitor = "";
+        path = "~/wallpapers/hyprlock.png";
+        blur_passes = 0;
+        contrast = 0.8916;
+        brightness = 0.8172;
+        vibrancy = 0.1696;
+        vibrancy_darkness = 0.0;
+      };
+      general = {
+        no_fade_in = false;
+        grace = 0;
+        disable_loading_bar = false;
+      };
+
+      label = [
+        {
+          monitor = "";
+          text = "Welcome!";
+          color = "rgba(216, 222, 233, .75)";
+          font_size = 55;
+          font_family = "SF Pro Display Bold";
+          position = "150, 320";
+          halign = "left";
+          valign = "center";
+        }
+
+        {
+          monitor = "";
+          text = "cmd[update:1000] echo \"<span>$(date +\"%I:%M\")</span>\"";
+          color = "rgba(216, 222, 233, .75)";
+          font_size = 40;
+          font_family = "SF Pro Display Bold";
+          position = "240, 240";
+          halign = "left";
+          valign = "center";
+        }
+
+        {
+          monitor = "";
+          text = "cmd[update:1000] echo -e \"$(date +\"%A, %B %d\")\"";
+          color = "rgba(216, 222, 233, .75)";
+          font_size = 19;
+          font_family = "SF Pro Display Bold";
+          position = "217, 175";
+          halign = "left";
+          valign = "center";
+        }
+        {
+          monitor = "";
+          text = "    $USER";
+          color = "rgba(216, 222, 233, 0.80)";
+          outline_thickness = 0;
+          dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+          dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0;
+          dots_center = true;
+          font_size = 16;
+          font_family = "SF Pro Display Bold";
+          position = "275, -140";
+          halign = "left";
+          valign = "center";
+        }
+      ];
+
+      # image = {
+      #   monitor = "";
+      #   path = "" ~/.config/hypr/vivek.png "";
+      #   border_size = 2;
+      #   border_color = "rgba(255, 255, 255, .75)";
+      #   size = 95;
+      #   rounding = -1;
+      #   rotate = 0;
+      #   reload_time = -1;
+      #   reload_cmd = "";
+      #   position = "270, 25";
+      #   halign = "left";
+      #   valign = "center";
+      # };
+
+      shape = {
+        monitor = "";
+        size = "320, 55";
+        color = "rgba(255, 255, 255, .1)";
+        rounding = -1;
+        border_size = 0;
+        border_color = "rgba(255, 255, 255, 1)";
+        rotate = 0;
+        xray = false; # if true, make a "hole" in the background (rectangle of specified size, no rotation)
+        position = "160, -140";
+        halign = "left";
+        valign = "center";
+      };
+
+      input-field = {
+        monitor = "";
+        size = "320, 55";
+        outline_thickness = 0;
+        dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8;
+        dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0;
+        dots_center = true;
+        outer_color = "rgba(255, 255, 255, 0)";
+        inner_color = "rgba(255, 255, 255, 0.1)";
+        font_color = "rgb(200, 200, 200)";
+        fade_on_empty = false;
+        font_family = "SF Pro Display Bold";
+        placeholder_text = "<i><span foreground=\"##ffffff99\">🔒  Enter Pass</span></i>";
+        hide_input = false;
+        position = "160, -220";
+        halign = "left";
+        valign = "center";
+      };
+    };
+  };
+
   programs.swaylock = {
     enable = true;
+    # nix swaylock doesn't play well with ubuntu pam
     package = pkgs.runCommandLocal "empty" { } "mkdir $out";
     settings = {
       color = "000000";
