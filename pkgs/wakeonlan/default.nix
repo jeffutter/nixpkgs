6de914c50d9fd494c6802b39d0cdac1b1fb3614c
Jeffery Utter <jeff@jeffutter.com>
2024-11-30 21:21:58 -0600

    Update cargoSha256 to cargoHash

--- pkgs/wakeonlan/default.nix
+++ pkgs/wakeonlan/default.nix
@@ -1,8 +1,13 @@
-{ stdenv, lib, fetchFromGitHub, rustPlatform }:
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
 
 let
   pname = "wakeonlan";
-  version = "0.1.0"; 
+  version = "0.1.0";
 in
 
 rustPlatform.buildRustPackage rec {
@@ -14,14 +19,14 @@ rustPlatform.buildRustPackage rec {
     rev = "v0.1.0";
     sha256 = "sha256-Ne4ZABsVNunAKoDoLflla5Tp0mMUmP1XHf1ijWRzjGs=";
   };
-    
-  cargoSha256 = "sha256-h51kf+4zfwUYOtbpCCMYIfp/AzHgdOCy5HPimiB3hlA=";
+
+  cargoHash = "sha256-h51kf+4zfwUYOtbpCCMYIfp/AzHgdOCy5HPimiB3hlA=";
 
   meta = with lib; {
     description = "Simple wake-on-lan program written in Rust";
     homepage = "https://github.com/jeffutter/wakeonlan-rust";
     license = licenses.asl20;
     platforms = platforms.unix;
-    hydraPlatforms = [];
+    hydraPlatforms = [ ];
   };
 }
