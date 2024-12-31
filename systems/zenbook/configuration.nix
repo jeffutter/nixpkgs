1f0543c32c44412f646333176ce8283ec6410575
Jeffery Utter <jeff@jeffutter.com>
2024-12-31 10:00:48 -0600

    Remove system-wide firefox from zenbook

--- systems/zenbook/configuration.nix
+++ systems/zenbook/configuration.nix
@@ -177,7 +177,7 @@
   programs.fish.enable = true;
 
   # Install firefox.
-  programs.firefox.enable = true;
+  # programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
