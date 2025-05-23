43197ab103d97ce0f3b52f281af679aa4b13ccd8
Jeffery Utter <jeff@jeffutter.com>
2025-05-22 23:22:01 -0500

    Changes for nix 25.05

--- systems/common.nix
+++ systems/common.nix
@@ -158,7 +158,7 @@ in
       # Elixir
       elixir
       elixir_ls
-      erlang_nox
+      beamMinimalPackages.erlang
 
       # Rust
       cargo
--- systems/zenbook/configuration.nix
+++ systems/zenbook/configuration.nix
@@ -146,7 +146,7 @@
   services.blueman.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
