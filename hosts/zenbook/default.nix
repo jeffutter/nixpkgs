6263e166aad15188115feb73468a14d665246137
Jeffery Utter <jeff@jeffutter.com>
2026-01-12 17:00:30 -0600

    Raise CPU_MAX_PERF_ON_BAT for zenbook

--- hosts/zenbook/default.nix
+++ hosts/zenbook/default.nix
@@ -267,7 +267,7 @@
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 60;
+      CPU_MAX_PERF_ON_BAT = 80;
 
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
